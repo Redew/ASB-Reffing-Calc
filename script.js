1601cfa90156d9fdd66a98fcbe56b474629060fd
CJ <cj7086@gmail.com>
2015-02-24 00:49:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,163 +1,200 @@
-/********THIS IS HOW TO WORK THIS PROGRAM. READ IT.********
-        Steps:
-            ctrl + a (highlights everything)
-            ctrl + c (copies everything highlighted)
-            ctrl + shift + j (opens the JavaScrpt console)
-            ctrl + v (pastes everything in the console)
-            EDIT THE RELEVANT DATA
-            PUSH ENTER
-            
-            IF YOU PUSH ENTER EARLY AND MESS UP, PUSH THE UP BUTTON AND CONTINUE EDITING
-*/
-
-// Base Attack Power. This is the base power of the move you are using.
-var bap = function (power) {
-    return power;
-};
-// stab() is automatically set to false. Set it to true if you want the STAB bonus.
 var stab = function (bonus) {
     if (bonus === true) {
-        bonus = 3;
+        return 3;
     } else {
-        bonus = 0;
-    };
-    return bonus;
+        return 0;
+    }
 };
-// crit() takes a few arguments. It randomly generates the crit chance for each stage (bar stage 3), 
-// and if it is true, returns 3. If left empty, it will return 0.
-// HOW TO EDIT CRIT CHANCE: make 'if (Math.random() <= 1 / 16) {' (for the relevant stage) = 'if (8 === 8) {' EXACTLY
+
 var crit = function (stage) {
     if (stage === 0) {
-        if(Math.random() <= 1 / 16) {
-            returnChance = 3;
-        } 
-    } 
-    else if (stage === 1) {
-        if(Math.random() <= 1 / 8) {
-            returnChance = 3
-        } 
-    } 
-    else if (stage === 2) {
-        if(Math.random() <= 1 / 2) {
-            returnChance = 3
+        if (Math.random() <= 1 / 16) {
+            return 3;
+        }
+    } else if (stage === 1) {
+        if (Math.random() <= 1 / 8) {
+            return 3;
+        }
+    } else if (stage === 2) {
+        if (Math.random() <= 1 / 2) {
+            return 3;
+        }
+    } else if (stage === 3) {
+        if (Math.random() < 1) {
+            return 3;
         }
     }
-    else if (stage === 3) {
-        if(Math.random() <= 1) {
-            returnChance = 3
-        }
-    } else {
-            returnChance = 0;
-    }
-    return returnChance;
+    return 0;
 };
-// You only need to enter in the base attack rank for the attacking Pokemon here. 
+
 var ATR = function (atkRank) {
+    if (!atkRank) atkRank = 0;
     if (atkRank <= 6) {
-        atkRank = atkRank * 1.5
-    } else {
+        atkRank = atkRank * 1.5;
+    } else if (atkRank > 6) {
         atkRank = (atkRank - 6) + (6 * 1.5);
-    };
+    }
     return atkRank;
 };
-// This works like typeEff(). The first argument is the string 'add' or 'subtract'.
-// If left blank it will just add 0. The second argument is what you want to add/subtract. 1, 3, 7, etc.
-var abilEff = function(subAdd, number) {
+
+var abilEff = function (subAdd, number) {
+    var toDo = 0;
     if (subAdd === 'subtract') {
-        toDo = - number;
+        toDo = -number;
     } else if (subAdd === 'add') {
-        toDo = + number;
-    } else {
-        toDo = + 0;
+        toDo = +number;
     }
     return toDo;
 };
-// Field effects take place when there is rain/sun/certain ASB fields/etc.
-var fieldEff = function(subAdd, number) {
+
+var fieldEff = function (subAdd, number) {
+    var toCalc = 0;
     if (subAdd === 'subtract') {
-        toCalc = - number;
+        toCalc = -number;
     } else if (subAdd === 'add') {
-        toCalc = + number;
-    } else {
-        toCalc = + 0;
+        toCalc = +number;
     }
     return toCalc;
 };
-// Input any number for a BAP boost/decrease. If left blank, adds 0.
-var itemEffOne = function(subAdd, number) {
+
+var itemEffOne = function (subAdd, number) {
+    var toCalc = 0;
     if (subAdd === 'subtract') {
-        toCalc = - number;
+        toCalc = -number;
     } else if (subAdd === 'add') {
-        toCalc = + number;
-    } else {
-        toCalc = + 0;
+        toCalc = +number;
     }
     return toCalc;
 };
-// Input any number for a power boost/decrease. If left blank, adds 0.
-var itemEffTwo = function(subAdd, number) {
+
+var itemEffTwo = function (subAdd, number) {
+    var toCalc = 0;
     if (subAdd === 'subtract') {
-        toCalc = - number;
+        toCalc = -number;
     } else if (subAdd === 'add') {
-        toCalc = + number;
-    } else {
-        toCalc = + 0;
+        toCalc = +number;
     }
     return toCalc;
 };
-// You only need to enter in the base defense rank for the defending Pokemon here. 
+
 var DRB = function (defRank) {
+    if (!defRank) defRank = 0;
     if (defRank <= 6) {
         defRank = defRank * 1.5;
-    } else {
+    } else if (defRank > 6) {
         defRank = (defRank - 6) + (6 * 1.5);
     }
     return defRank;
 };
-// If left blank, this is false (adds 0). If true, it subtracts 3.
+
 var burnEff = function (bonus) {
     if (bonus === true) {
-        answer = 3;
+        return 3;
     } else {
-        answer = 0;
-    };
-    return answer;
+        return 0;
+    }
 };
-// When entering in the effectiveness, you need to put it in quotation marks. i.e. typeEff('weak') 
-// typeEff('triple resist')  etc.
-// Note: I'M STILL WORKING ON THIS PART. SOME OF IT MAY BE BUGGY.
+
 var typeEff = function (effectiveness) {
-    var goThrough = function(numb) {
+    var goThrough = function (numb) {
         var show = numb * 100;
         var doThis = Math.round(show);
         return doThis / 100;
     };
-    if (effectiveness === 'weak') {
+    var returnThis = 1;
+    if (effectiveness === '2') {
         returnThis = 3 / 2;
-    } else if (effectiveness === 'double weak') {
+    } else if (effectiveness === '4') {
         returnThis = 9 / 4;
-    } else if (effectiveness === 'triple weak') {
+    } else if (effectiveness === '8') {
         returnThis = 27 / 8;
-    } else if (effectiveness === 'resist') {
-        returnThis = 2 / 3
+    } else if (effectiveness === '1/2') {
+        returnThis = 2 / 3;
         goThrough(returnThis);
-    } else if (effectiveness === 'double resist') {
+    } else if (effectiveness === '1/4') {
         returnThis = 0.44;
-    } else if (effectiveness === 'triple resist') {
+    } else if (effectiveness === '1/8') {
         returnThis = 0.30;
-    } else {
-        returnThis = 1;
     }
     return returnThis;
 };
-// Stage Boost Difference is the difference between the attacker's relevant Attack Stage Boosts 
-// and the defender's Defense Stage Boosts
-var SBD = function (boostDiff) {
-    return boostDiff;
+
+var SBD = function (numOne, numTwo) {
+    var atkDiff = numOne || 0;
+    var defDiff = numTwo || 0;
+    if (critVal) {
+        if (atkDiff < 0) atkDiff = 0;
+        if (defDiff > 0) defDiff = 0;
+    }
+    return atkDiff - defDiff;
 };
 
-// THIS IS WHAT YOU NEED TO EDIT. ONLY TOUCH THIS
-var finalDmg = ((bap() + stab() + crit() + ATR() + abilEff() + fieldEff() + itemEffOne() - DRB() - burnEff()) * typeEff()) + (SBD() * 2) + itemEffTwo();
+var calculate = function() {
+    var basePower = parseInt($('.bap').val()) || 0;
+    var stabBonus = $('.stabBonus').prop('checked');
+    var critStage = parseInt($('.critHold').val()) || 0;
+    var atkRank = parseInt($('.ATR').val()) || 0;
+    var abilityEffect = $('input[name=addSubAbility][value="add"]').prop("checked") ? 'add' : 'subtract'
+    var abilityEffVal = parseInt($('.abilityEffectModifier').val()) || 0;
+    var fieldEffect = $('input[name=addSubField][value="add"]').prop("checked") ? 'add' : 'subtract'
+    var fieldEffVal = parseInt($('.fieldEffectModifier').val()) || 0;
+    var itemEffectOne = $('input[name=addSubItemOne][value="add"]').prop("checked") ? 'add' : 'subtract'
+    var itemEffOneVal = parseInt($('.itemEffectModifierOne').val()) || 0;
+    var itemEffectTwo = $('input[name=addSubItemTwo][value="add"]').prop("checked") ? 'add' : 'subtract'
+    var itemEffTwoVal = parseInt($('.itemEffectModifierTwo').val()) || 0;
+    var defRank = parseInt($('.DRB').val()) || 0;
+    var burnCheck = $('.burnEffect').prop('checked');
+    var typeEffectivenessButton = $('input:radio[name=effect]:checked') ? $('input:radio[name=effect]:checked').val() : 1
+    var atkStageBoosts = parseInt($('.attackStageBoosts').val()) || 0;
+    var defStageBoosts = parseInt($('.defenseStageBoosts').val()) || 0;
+    
+    critVal = crit(critStage);
+    
+    var finalDmg = typeEffectivenessButton === '0' ? 0 : ((basePower + stab(stabBonus) + critVal + ATR(atkRank) + abilEff(abilityEffect, abilityEffVal) + fieldEff(fieldEffect, fieldEffVal) + itemEffOne(itemEffectOne, itemEffOneVal) - DRB(defRank) - burnEff(burnCheck)) * typeEff(typeEffectivenessButton)) + (SBD(atkStageBoosts, defStageBoosts) * 2) + itemEffTwo(itemEffectTwo,itemEffTwoVal);
+    $('.output').text(finalDmg);
+}
+ var resetCalc = function() {
+    $('.bap').val("");
+    $('.stabBonus').prop("checked", false);
+    $('.critHold').val("");
+    $('.ATR').val("");
+    $('input[name="addSubAbility"][value="add"]').prop("checked", true);
+    $('.abilityEffectModifier').val("");
+    $('input[name="addSubField"][value="add"]').prop("checked", true);
+    $('.fieldEffectModifier').val("");
+    $('input[name="addSubItemOne"][value="add"]').prop("checked", true);
+    $('.itemEffectModifierOne').val("");
+    $('input[name="addSubItemTwo"][value="add"]').prop("checked", true);
+    $('.itemEffectModifierTwo').val("");
+    $('.DRB').val("");
+    $('.burnEffect').prop("checked", false);
+    $('input[name="effect"][value="1"]').prop("checked", true);
+    $('.attackStageBoosts').val("");
+    $('.defenseStageBoosts').val("");
+ }
 
-// This outputs the final number.
-console.log(finalDmg);
+$(document).ready(function () {
+
+    resetCalc();
+    $('#contributors').hide();
+    $('#contributors').css({
+        left: '580px',
+        top: '250px',
+        width: 0,
+        height: 0
+    });
+    $('.contribs').click(function () {
+        $('#contributors').toggle(function () {
+            $('#contributors').animate({
+                left: '420px',
+                top: '200px',
+                width: '400px',
+                height: '250px',
+                opacity: 1
+            });
+        });
+    });
+
+    $('.calcButton').click(calculate);    
+    $('.resetButton').click(resetCalc);
+});
